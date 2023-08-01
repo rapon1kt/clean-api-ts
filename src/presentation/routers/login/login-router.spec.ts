7b40372a96f3eeb63aa1a33b31f537186778c555
rapon1kt <shinnatensei@gmail.com>
2023-07-31 22:01:00 -0300

    feat: ensure login router returns 400 if no password is provided

--- src/presentation/routers/login/login-router.spec.ts
+++ src/presentation/routers/login/login-router.spec.ts
@@ -9,7 +9,7 @@ interface HttpRequest {
 
 class LoginRouter {
 	route(httpRequest: HttpRequest) {
-		if (!httpRequest.body.email) {
+		if (!httpRequest.body.email || !httpRequest.body.password) {
 			return {
 				statusCode: 400,
 			};
@@ -24,6 +24,18 @@ describe("login router", () => {
 			body: {
 				password: "any_password",
 				email: undefined,
+			},
+		};
+		const httpResponse = sut.route(httpRequest);
+		expect(httpResponse.statusCode).toBe(400);
+	});
+
+	test("should return 400 with no password is provided", () => {
+		const sut = new LoginRouter();
+		const httpRequest = {
+			body: {
+				password: undefined,
+				email: "any_email@email.com",
 			},
 		};
 		const httpResponse = sut.route(httpRequest);
