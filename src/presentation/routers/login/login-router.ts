204b3de0b6745c2ad45de59fd90833c6babe3f56
rapon1kt <shinnatensei@gmail.com>
2023-08-01 18:43:45 -0300

    refactor: move login router to its own file

---
+++ src/presentation/routers/login/login-router.ts
@@ -0,0 +1,23 @@
+import HttpResponse from "../../helpers/http-response/http-response";
+
+interface HttpRequest {
+	body: {
+		email: string;
+		password: string;
+	};
+}
+
+export default class LoginRouter {
+	route(httpRequest: HttpRequest) {
+		if (!httpRequest || !httpRequest.body) {
+			return HttpResponse.serverError();
+		}
+		const { email, password } = httpRequest.body;
+		if (!email) {
+			return HttpResponse.badRequest("email");
+		}
+		if (!password) {
+			return HttpResponse.badRequest("password");
+		}
+	}
+}
