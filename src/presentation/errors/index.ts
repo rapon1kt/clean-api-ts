aa097b8bec9320abd986ae30c885a29f834f5b48
rapon1kt <shinnatensei@gmail.com>
2023-08-01 18:46:09 -0300

    feat: export errors from errors folder

---
+++ src/presentation/errors/index.ts
@@ -0,0 +1,2 @@
+export { default as MissingParamError } from "./missing-param-error/missing-param-error";
+export { default as InternalServerError } from "./internal-server-error/internal-server-error";
